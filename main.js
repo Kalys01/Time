0a37692a5fbacdf8b6c0f36dd5c454d7fb052f7d
Kalys01 <kalysosmon96.g@gmail.com>
2021-01-12 23:28:51 +0600

    added days of the week

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 function dataTimes(){
     const times = document.querySelector('.times');
     const dates = document.querySelector('.dates');
+    const days = document.querySelector('.day');
 
     let data = new Date();
 
@@ -14,6 +15,19 @@ function dataTimes(){
 
     times.innerHTML = ( ("0" + hour).slice(-2) + ":" + ("0" + min).slice(-2) + ":" + ("0" + sec).slice(-2) );
     dates.innerHTML = ( year + "/" + ("0" + month + 1).slice(-2) + "/" + ("0" + day).slice(-2) );
+
+    let d = data.getDay();
+    let week = '';
+    switch (d) {
+        case 0: week = "Sanday"; break;
+        case 1: week = "Monday"; break;
+        case 2: week = "Tuesday"; break;
+        case 3: week = "Wednesday"; break;
+        case 4: week = "Thursday"; break;
+        case 5: week = "Friday"; break;
+        case 6: week = "Saturday"; break;
+    }
+    days.innerHTML = week;
 }
 
 window.onload = function(){
