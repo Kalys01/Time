92a6c93fbdf2267b713d2cdf3ee71b0465a3b1af
Kalys01 <kalysosmon96.g@gmail.com>
2021-02-03 02:02:53 +0600

    corrected month

--- main.js
+++ main.js
@@ -10,22 +10,22 @@ function dataTimes(){
     let sec = data.getSeconds();
 
     let year = data.getFullYear();
-    let month = data.getMonth();
+    let month = data.getMonth() + 1;
     let day = data.getDate();
 
     times.innerHTML = ( ("0" + hour).slice(-2) + ":" + ("0" + min).slice(-2) + ":" + ("0" + sec).slice(-2) );
-    dates.innerHTML = ( year + "/" + ("0" + month + 1).slice(-2) + "/" + ("0" + day).slice(-2) );
+    dates.innerHTML = ( year + "/" + ("0" + month).slice(-2) + "/" + ("0" + day).slice(-2) );
 
     let d = data.getDay();
     let week = '';
     switch (d) {
-        case 0: week = "Sanday"; break;
-        case 1: week = "Monday"; break;
-        case 2: week = "Tuesday"; break;
-        case 3: week = "Wednesday"; break;
-        case 4: week = "Thursday"; break;
-        case 5: week = "Friday"; break;
-        case 6: week = "Saturday"; break;
+        case 0: week = "Жекшемби"; break;
+        case 1: week = "Дүйшөмбү"; break;
+        case 2: week = "Шейшемби"; break;
+        case 3: week = "Шаршемби"; break;
+        case 4: week = "Бейшенби"; break;
+        case 5: week = "Жума"; break;
+        case 6: week = "Ишемби"; break;
     }
     days.innerHTML = week;
 }
